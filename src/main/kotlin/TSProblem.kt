334b9a7d8b2957d4280e3c3681755ae460ae89c3
rswc <rshoryti@protonmail.com>
2024-03-13 16:47:24 +0100

    Fix TSProblem code style

--- src/main/kotlin/TSProblem.kt
+++ src/main/kotlin/TSProblem.kt
@@ -1,21 +1,22 @@
 package org.example
 
+import kotlin.math.pow
 import kotlin.math.roundToInt
+import kotlin.math.sqrt
 
 class TSProblem(val points: List<List<Int>>) {
 
     val distanceMatrix: Array<IntArray>
-    val dimension: Int
+    val dimension: Int = points.size
 
     init {
-        dimension = points.size
         distanceMatrix = Array(dimension) {i ->
             IntArray(dimension) {j ->
                 points[i]
                     .zip(points[j])
-                    .map { Math.pow((it.first - it.second).toDouble(), 2.0) }
+                    .map { (it.first - it.second).toDouble().pow(2.0) }
                     .reduce {acc, d -> acc + d}
-                    .let { Math.sqrt(it) }
+                    .let { sqrt(it) }
                     .roundToInt()
             }
         }
