796d12fae99c2682c2355ae1416cd46fb2a5f551
rswc <rshoryti@protonmail.com>
2024-04-05 20:06:12 +0200

    Finish basic greedy local search

--- src/main/kotlin/local/LocalSearch.kt
+++ src/main/kotlin/local/LocalSearch.kt
@@ -13,35 +13,88 @@ class LocalSearch(private val presolver: ISolver<TSProblem>): ISolver<TSProblem>
         val startNodes = listOf(0, 1).flatMap { i -> cycles[i].indices.map { it to i } }.toMutableList()
         val endNodes = startNodes.toMutableList()
 
-        startNodes.shuffle()
-        endNodes.shuffle()
+        do {
+            var delta: Int
+            var swapped = false
 
-        for (start in startNodes) {
-            for (end in endNodes) {
-                if (start.second != end.second) {
-                    // intercycle
+            startNodes.shuffle()
+            endNodes.shuffle()
 
-                    val delta = dm[end.first][(start.first - 1).mod(cycles[start.second].size)] +
-                            dm[end.first][(start.first + 1).mod(cycles[start.second].size)] -
-                            dm[start.first][(start.first - 1).mod(cycles[start.second].size)] -
-                            dm[start.first][(start.first + 1).mod(cycles[start.second].size)] +
-                            dm[start.first][(end.first - 1).mod(cycles[end.second].size)] +
-                            dm[start.first][(end.first + 1).mod(cycles[end.second].size)] -
-                            dm[end.first][(end.first - 1).mod(cycles[end.second].size)] -
-                            dm[end.first][(end.first + 1).mod(cycles[end.second].size)]
+            for (start in startNodes) {
+                for (end in endNodes) {
+                    if (start.second != end.second) {
+                        // intercycle
 
-                    if (delta > 0) {
-                        // TODO: commit vertex swap and restart loop
+                        val startPrev = cycles[start.second][(start.first - 1).mod(cycles[start.second].size)]
+                        val startNode = cycles[start.second][start.first]
+                        val startNext = cycles[start.second][(start.first + 1).mod(cycles[start.second].size)]
+
+                        val endPrev = cycles[end.second][(end.first - 1).mod(cycles[end.second].size)]
+                        val endNode = cycles[end.second][end.first]
+                        val endNext = cycles[end.second][(end.first + 1).mod(cycles[end.second].size)]
+
+                        delta = dm[startNode][endPrev] +
+                                dm[startNode][endNext] +
+                                dm[endNode][startPrev] +
+                                dm[endNode][startNext] -
+                                dm[startNode][startPrev] -
+                                dm[startNode][startNext] -
+                                dm[endNode][endPrev] -
+                                dm[endNode][endNext]
+
+                        // When the two nodes are next to each other, they share an edge
+                        if (startNode == endNext) {
+                            delta += dm[startNode][startPrev] + dm[startNode][endNode]
+                        }
+                        if (endNode == startNext) {
+                            delta += dm[endNode][endPrev] + dm[endNode][startNode]
+                        }
+
+                        if (delta < 0) {
+                            cycles[start.second][start.first] = cycles[end.second][end.first]
+                                .also { cycles[end.second][end.first] = cycles[start.second][start.first] }
+                            swapped = true
+                        }
+
+                    } else {
+                        // intracycle
+
+                        val startPrev = cycles[start.second][(start.first - 1).mod(cycles[start.second].size)]
+                        val startNode = cycles[start.second][start.first]
+                        val startNext = cycles[start.second][(start.first + 1).mod(cycles[start.second].size)]
+
+                        val endPrev = cycles[end.second][(end.first - 1).mod(cycles[end.second].size)]
+                        val endNode = cycles[end.second][end.first]
+                        val endNext = cycles[end.second][(end.first + 1).mod(cycles[end.second].size)]
+
+                        delta = dm[startNode][endPrev] +
+                                dm[startNode][endNext] +
+                                dm[endNode][startPrev] +
+                                dm[endNode][startNext] -
+                                dm[startNode][startPrev] -
+                                dm[startNode][startNext] -
+                                dm[endNode][endPrev] -
+                                dm[endNode][endNext]
+
+                        // When the two nodes are next to each other, they share an edge
+                        if (startNode == endNext) {
+                            delta += dm[startNode][startPrev] + dm[startNode][endNode]
+                        }
+                        if (endNode == startNext) {
+                            delta += dm[endNode][endPrev] + dm[endNode][startNode]
+                        }
+
+                        if (delta < 0) {
+                            cycles[start.second][start.first] = cycles[end.second][end.first]
+                                .also { cycles[end.second][end.first] = cycles[start.second][start.first] }
+                            swapped = true
+                        }
                     }
-
-                } else {
-                    // intracycle
-                    // TODO: do vertex or edge swap, depending on configuration
                 }
             }
-        }
+        } while (swapped)
 
-        return TSPSolution(instance, mutableListOf(), mutableListOf())
+        return TSPSolution(instance, cycles[0], cycles[1])
     }
 
     override fun getDisplayName(): String {
