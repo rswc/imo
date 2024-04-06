7fd39bc2ebc9e3016a56d530e7c30a9c5835c0b5
rswc <rshoryti@protonmail.com>
2024-04-06 16:21:00 +0200

    Add local search baseline

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 package org.example
 
 import org.example.core.Experiment
+import org.example.local.BaselineLocalSearch
 import org.example.local.GreedyLocalSearch
 import org.example.local.SteepLocalSearch
 import org.example.tsp.*
@@ -10,14 +11,16 @@ fun main() {
 
     val experiment = Experiment(
         listOf(
-            GreedyLocalSearch(RandomSolver(), swapEdges = false),
-            GreedyLocalSearch(RandomSolver(), swapEdges = true),
-            GreedyLocalSearch(bestGreedySolver, swapEdges = false),
-            GreedyLocalSearch(bestGreedySolver, swapEdges = true),
-            SteepLocalSearch(RandomSolver(), swapEdges = false),
-            SteepLocalSearch(RandomSolver(), swapEdges = true),
-            SteepLocalSearch(bestGreedySolver, swapEdges = false),
-            SteepLocalSearch(bestGreedySolver, swapEdges = true),
+//            GreedyLocalSearch(RandomSolver(), swapEdges = false),
+//            GreedyLocalSearch(RandomSolver(), swapEdges = true),
+//            GreedyLocalSearch(bestGreedySolver, swapEdges = false),
+//            GreedyLocalSearch(bestGreedySolver, swapEdges = true),
+//            SteepLocalSearch(RandomSolver(), swapEdges = false),
+//            SteepLocalSearch(RandomSolver(), swapEdges = true),
+//            SteepLocalSearch(bestGreedySolver, swapEdges = false),
+//            SteepLocalSearch(bestGreedySolver, swapEdges = true),
+            BaselineLocalSearch(RandomSolver(), 32L),
+            BaselineLocalSearch(bestGreedySolver, 32L),
         ),
         listOf(TSPReader.read("/kroA100.tsp"), TSPReader.read("/kroB100.tsp"))
     )
---
+++ src/main/kotlin/local/BaselineLocalSearch.kt
@@ -0,0 +1,121 @@
+package org.example.local
+
+import org.example.core.ISolver
+import org.example.tsp.TSPSolution
+import org.example.tsp.TSProblem
+
+class BaselineLocalSearch(private val presolver: ISolver<TSProblem>, private val timeLimit: Long):
+    ISolver<TSProblem> {
+    override fun solve(instance: TSProblem, experimentStep: Int?): TSPSolution {
+        val targetTime = System.currentTimeMillis() + timeLimit
+        val dm = instance.distanceMatrix
+        val initialSolution = presolver.solve(instance) as TSPSolution
+        val cycles = listOf(initialSolution.cycleA, initialSolution.cycleB)
+
+        val nodeToCycle = MutableList(instance.dimension) { 0 }
+        for (node in cycles[1]) {
+            nodeToCycle[node] = 1
+        }
+
+        var bestSolution = initialSolution
+        var bestScore = initialSolution.score()
+        var currentScore = bestScore
+
+        val nodes = listOf(0, 1).flatMap { i -> cycles[i].indices.map { it to i } }.toMutableList()
+
+        do {
+            var delta: Int
+            val start = nodes.random()
+            val end = nodes.random()
+
+            val cycleStart = start.second
+            val cycleEnd = end.second
+
+            if (start == end) {
+                continue
+            }
+
+            if (cycleStart == cycleEnd && (0..1).random() == 1) {
+                // intracycle edge swap
+
+                val startPrev = cycles[cycleStart][(start.first - 1).mod(cycles[cycleStart].size)]
+                val startNode = cycles[cycleStart][start.first]
+
+                val endNode = cycles[cycleEnd][end.first]
+                val endNext = cycles[cycleEnd][(end.first + 1).mod(cycles[cycleEnd].size)]
+
+                if (endNext == startNode) {
+                    continue
+                }
+
+                delta = dm[startPrev][endNode] +
+                        dm[startNode][endNext] -
+                        dm[startPrev][startNode] -
+                        dm[endNode][endNext]
+
+                var n = end.first - start.first + 1
+
+                if (n < 0) {
+                    n += instance.dimension
+                }
+
+                n /= 2
+
+                for (i in 0 until n) {
+                    val left = (start.first + i).mod(cycles[cycleEnd].size)
+                    val right = (end.first - i).mod(cycles[cycleEnd].size)
+
+                    cycles[cycleEnd][left] = cycles[cycleEnd][right].also { cycles[cycleEnd][right] = cycles[cycleEnd][left] }
+                }
+
+            } else {
+                // vertex swap
+
+                val startPrev = cycles[cycleStart][(start.first - 1).mod(cycles[cycleStart].size)]
+                val startNode = cycles[cycleStart][start.first]
+                val startNext = cycles[cycleStart][(start.first + 1).mod(cycles[cycleStart].size)]
+
+                val endPrev = cycles[cycleEnd][(end.first - 1).mod(cycles[cycleEnd].size)]
+                val endNode = cycles[cycleEnd][end.first]
+                val endNext = cycles[cycleEnd][(end.first + 1).mod(cycles[cycleEnd].size)]
+
+                delta = dm[startNode][endPrev] +
+                        dm[startNode][endNext] +
+                        dm[endNode][startPrev] +
+                        dm[endNode][startNext] -
+                        dm[startNode][startPrev] -
+                        dm[startNode][startNext] -
+                        dm[endNode][endPrev] -
+                        dm[endNode][endNext]
+
+                // When the two nodes are next to each other, they share an edge
+                if (startNode == endNext) {
+                    delta += dm[startNode][startPrev] + dm[startNode][endNode]
+                }
+                if (endNode == startNext) {
+                    delta += dm[endNode][endPrev] + dm[endNode][startNode]
+                }
+
+                cycles[cycleStart][start.first] = cycles[cycleEnd][end.first]
+                    .also { cycles[cycleEnd][end.first] = cycles[cycleStart][start.first] }
+
+                nodeToCycle[start.first] = cycleEnd.also { nodeToCycle[end.first] = cycleStart }
+            }
+
+            currentScore += delta
+
+            if (currentScore < bestScore) {
+                bestScore = currentScore
+                bestSolution = TSPSolution(instance, cycles[0].toMutableList(), cycles[1].toMutableList())
+            }
+
+        } while (System.currentTimeMillis() < targetTime)
+
+        return bestSolution
+    }
+
+    override fun getDisplayName(): String {
+        return "Baseline & ${presolver.getDisplayName()}"
+    }
+
+}
